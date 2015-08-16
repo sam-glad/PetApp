b39e0c3a89fa9308a1d7fde8b3c794c09246cc61
Sam Gladstone <gladston@umich.edu>
2015-08-16 17:20:03 -0400

    Add valid, invalid organization attributes for controller test

--- spec/controllers/organizations_controller_spec.rb
+++ spec/controllers/organizations_controller_spec.rb
@@ -24,11 +24,11 @@ RSpec.describe OrganizationsController, :type => :controller do
   # Organization. As you add validations to Organization, be sure to
   # adjust the attributes here as well.
   let(:valid_attributes) {
-    skip("Add a hash of attributes valid for your model")
+    { name: 'Testing foobar' }
   }
 
   let(:invalid_attributes) {
-    skip("Add a hash of attributes invalid for your model")
+    { name: ' ' }
   }
 
   # This should return the minimal set of values that should be in the session

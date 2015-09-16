04ea13994a12b273037e35da799d70302fe2889c
Sam Gladstone <gladston@umich.edu>
2015-09-16 15:25:52 -0400

    User must sign in before CUD'ing organizations

--- app/controllers/organizations_controller.rb
+++ app/controllers/organizations_controller.rb
@@ -1,5 +1,6 @@
 class OrganizationsController < ApplicationController
   before_action :set_organization, only: [:show, :update, :destroy]
+  before_filter :authenticate_user!, :only => [:create, :update, :destroy]
 
   # GET /organizations
   # GET /organizations.json

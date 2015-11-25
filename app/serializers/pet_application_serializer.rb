4a40d0b623655cca8a6a6e3391f2bae1576f96f9
Sam Gladstone <gladston@umich.edu>
2015-11-25 18:11:40 -0500

    Update PetApplications serializer

--- app/serializers/pet_application_serializer.rb
+++ app/serializers/pet_application_serializer.rb
@@ -1,6 +1,8 @@
 class PetApplicationSerializer < ActiveModel::Serializer
-  attributes :id, :pet_id
+  attributes :id, :pet_id, :status, :created_at
 
   has_many :questions
   has_one :organization
+  has_one :user
+  has_one :pet
 end

356f31b27ea220725ce8c6f8c91bf793f9a3fecb
Sam Gladstone <gladston@umich.edu>
2016-01-14 00:20:38 -0500

    Add controller test for pet permissions

--- spec/controllers/pets_controller_spec.rb
+++ spec/controllers/pets_controller_spec.rb
@@ -2,11 +2,15 @@ require 'rails_helper'
 
 RSpec.describe PetsController, :type => :controller do
 
-  # This should return the minimal set of attributes required to create a valid
-  # Pet. As you add validations to Pet, be sure to
-  # adjust the attributes here as well.
-  let(:valid_attributes) {
-    {
+  def valid_attributes(user = nil, user_can_edit_pets = false)
+    organization = FactoryGirl.create(:organization)
+    user ||= FactoryGirl.create(:user)
+    if user_can_edit_pets
+      OrganizationMembership.create(user_id: user.id,
+        organization_id: organization.id, can_edit_all_pets: true)
+    end
+
+    return {
       animal: 'Dog',
       is_mix?: false,
       age: 'Young',
@@ -17,26 +21,38 @@ RSpec.describe PetsController, :type => :controller do
       good_with_dogs: true,
       good_with_cats: false,
       good_with_kids: false,
+      is_adoptable: true,
+      is_fosterable: true,
       breeds: [FactoryGirl.create(:breed)],
-      foster_application_id: ApplicationForm.create.id
+      foster_application_id: ApplicationForm.create.id,
+      organization: organization
     }
-  }
+  end
 
-  let(:invalid_attributes) {
-    {
-      foster_application_id: ApplicationForm.create.id
-    }
-  }
+  def invalid_attributes
+    return { foster_application_id: ApplicationForm.create.id }
+  end
 
-  # This should return the minimal set of values that should be in the session
-  # in order to pass any filters (e.g. authentication) defined in
-  # PetsController. Be sure to keep this updated too.
-  let(:valid_session) { {} }
+  def new_attributes
+    return { is_adoptable: false }
+  end
+
+  # TODO: Move this somewhere for common access
+  def login(user)
+    @request.env['devise.mapping'] = Devise.mappings[:user]
+    sign_in(user)
+  end
+
+  let(:user) { FactoryGirl.create(:user) }
+
+  before(:each) do
+    login(user)
+  end
 
   describe "GET index" do
     it "assigns all pets as @pets" do
       pet = Pet.create! valid_attributes
-      get :index, {}, valid_session
+      get :index, {}
       expect(assigns(:pets)).to eq([pet])
     end
   end
@@ -44,63 +60,53 @@ RSpec.describe PetsController, :type => :controller do
   describe "GET show" do
     it "assigns the requested pet as @pet" do
       pet = Pet.create! valid_attributes
-      get :show, {:id => pet.to_param}, valid_session
+      get :show, {:id => pet.to_param}
       expect(assigns(:pet)).to eq(pet)
     end
   end
 
-  # describe "POST create" do
-  #   describe "with valid params" do
-  #     it "creates a new Pet" do
-  #       expect {
-  #         post :create, {:pet => valid_attributes}, valid_session
-  #       }.to change(Pet, :count).by(1)
-  #     end
-  #
-  #     it "assigns a newly created pet as @pet" do
-  #       post :create, {:pet => valid_attributes}, valid_session
-  #       expect(assigns(:pet)).to be_a(Pet)
-  #       expect(assigns(:pet)).to be_persisted
-  #     end
-  #   end
-  #
-  #   describe "with invalid params" do
-  #     it "assigns a newly created but unsaved pet as @pet" do
-  #       post :create, {:pet => invalid_attributes}, valid_session
-  #       expect(assigns(:pet)).to be_a_new(Pet)
-  #     end
-  #   end
-  # end
+  describe 'PUT update' do
+    context 'with an authorized user' do
+      describe 'with valid params' do
+        it 'updates the requested pet' do
+          pet = Pet.create! valid_attributes(user, true)
+          put :update, {:id => pet.to_param, :pet => new_attributes}
+          pet.reload
+          new_attributes.each_pair do |key, value|
+            expect(pet[key]).to eq(value)
+          end
+        end
 
-  describe "PUT update" do
-    describe "with valid params" do
-      let(:new_attributes) {
-        {
-          foster_application_id: ApplicationForm.create.id
-        }
-      }
-
-      it "updates the requested pet" do
-        pet = Pet.create! valid_attributes
-        put :update, {:id => pet.to_param, :pet => new_attributes}, valid_session
-        pet.reload
-        new_attributes.each_pair do |key, value|
-          expect(pet[key]).to eq(value)
+        it 'assigns the requested pet as @pet' do
+          pet = Pet.create! valid_attributes(user, true)
+          put :update, {:id => pet.to_param, :pet => valid_attributes}
+          expect(assigns(:pet)).to eq(pet)
         end
       end
 
-      it "assigns the requested pet as @pet" do
-        pet = Pet.create! valid_attributes
-        put :update, {:id => pet.to_param, :pet => valid_attributes}, valid_session
-        expect(assigns(:pet)).to eq(pet)
+      describe 'with invalid params' do
+        it 'assigns the pet as @pet' do
+          pet = Pet.create! valid_attributes(user, true)
+          put :update, {:id => pet.to_param, :pet => invalid_attributes}
+          expect(assigns(:pet)).to eq(pet)
+        end
       end
     end
 
-    describe "with invalid params" do
-      it "assigns the pet as @pet" do
+    context 'with an unauthorized user' do
+      it 'returns 403 Forbidden' do
         pet = Pet.create! valid_attributes
-        put :update, {:id => pet.to_param, :pet => invalid_attributes}, valid_session
-        expect(assigns(:pet)).to eq(pet)
+        put :update, {:id => pet.to_param, :pet => valid_attributes}
+        expect(response).to have_http_status(403)
+      end
+
+      it 'fails to update the requested pet' do
+        pet = Pet.create! valid_attributes
+        put :update, {:id => pet.to_param, :pet => new_attributes}
+        pet.reload
+        new_attributes.each_pair do |key, value|
+          expect(pet[key]).not_to eq(value)
+        end
       end
     end
   end
@@ -109,7 +115,7 @@ RSpec.describe PetsController, :type => :controller do
     it "destroys the requested pet" do
       pet = Pet.create! valid_attributes
       expect {
-        delete :destroy, {:id => pet.to_param}, valid_session
+        delete :destroy, {:id => pet.to_param}
       }.to change(Pet, :count).by(-1)
     end
   end

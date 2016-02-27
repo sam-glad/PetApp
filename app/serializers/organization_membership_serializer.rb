d516394114d442c508cd2781567c10f92421c3ae
Sam Gladstone <gladston@umich.edu>
2016-02-26 20:46:04 -0500

    camelCase serializer attributes

--- app/serializers/organization_membership_serializer.rb
+++ app/serializers/organization_membership_serializer.rb
@@ -1,3 +1,15 @@
 class OrganizationMembershipSerializer < ActiveModel::Serializer
-  attributes :id, :organization_id, :user_id, :is_admin
+  attributes :id, :organizationId, :userId, :isAdmin
+
+  def organizationId
+    return object.organization_id
+  end
+
+  def userId
+    return object.user_id
+  end
+
+  def isAdmin
+    return object.is_admin
+  end
 end
